2eda6a1d851b6968e35e28396f45527da4856117
reza <reza.reza@gmail.com>
2017-05-13 10:26:10 -0700

    -

---
+++ DanaManager/DanaManager/TsTmService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanaManager
+{
+    class TsTmService
+    {
+        //http://membersseo.tsetmc.com/tsev2/data/InstTradeHistory.aspx?i=41048299027409941&Top=999999&A=0
+        //20170513@3960.00@3870.00@3906.00@3896.00@3900.00@3902.00@957447408.00@244178@56;
+        //20170510@3922.00@3850.00@3902.00@3899.00@3897.00@3768.00@9865052094.00@2528207@199;
+        //20170509@3900.00@3648.00@3768.00@3840.00@3800.00@3840.00@26816131046.00@7116564@452;
+        //20170508@3950.00@3800.00@3840.00@3800.00@3852.00@3905.00@9786054081.00@2548730@190;
+        //Date@High@Low@Payani@Close@Open@Yesterday@Arzesh@Volume@Tedad;
+
+
+    }
+}
